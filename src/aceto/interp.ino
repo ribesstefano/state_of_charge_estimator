25fde5db09256ad918dc212b10c8a271e9dde9f4
stefanoribes <ribes.stefano@gmail.com>
2021-02-01 20:33:36 +0100

    Added interpolation and removed loop on SD

--- src/aceto/interp.ino
+++ src/aceto/interp.ino
@@ -3,28 +3,28 @@
  * Is using malloc() and free() a bad idea in Arduino?
  *  Reference: https://arduino.stackexchange.com/questions/682/is-using-malloc-and-free-a-really-bad-idea-on-arduino
  */
-
+// TODO: To read a float from a binary value.
 float snelheid;
-
 union u_tag {
    byte b[4];
    float fval;
 } u;
 
-const float coeff[] = {0.0, 0.0, 0.0, 0.0};
-const int kNumCoeffs = 4;
+// NOTE: The zero coefficient starts at index zero.
+// const float coeff[] = {10.465, 0.0262};
+const float coeff[] = {-398.0, 38.042};
+const int kNumCoeffs = sizeof(coeff) / sizeof(float);
 
 float interpolate(const float x_in) {
+  // TODO: To read a float from a binary value.
   // u.b[0] = snelheidArray[0];
   // u.b[1] = snelheidArray[1];
   // u.b[2] = snelheidArray[2];
   // u.b[3] = snelheidArray[3];
   // snelheid = u.fval;
-  
   float x = x_in;
   float y = coeff[0];
-  // NOTE: We start from the second coefficient.
-  for (int i = 1; i < kNumCoeffs; ++i) {
+  for (int i = 1; i < kNumCoeffs; ++i) { // We start from the second coefficient
      y += x * coeff[i];
      x *= x;
   }
--- src/aceto/read_avg_adc.ino
+++ src/aceto/read_avg_adc.ino
@@ -1,18 +1,17 @@
 /*
-  Analog Input Reads shunt current and battery tension
-*/
+ * Analog Input Reads shunt current and battery tension
+ */
 const float kBatteryCapacity = 12.0; // Ampere-hour
-const float kBatteryVoltage = 12.66; // Measured voltage with load on. In Volt.
 const float kMeasuredCircuitCurrent = 1.81;
 const float kMeasuredShuntVoltage = 0.027;
 const float kShuntRes = kMeasuredShuntVoltage / kMeasuredCircuitCurrent; // Ohm
-const float kScalerADC = 5.0 / 1023.0;
-const float kScalerPartit = 3.07;
+const float kScalerPartit = 51.45; // 3.07; 
 const float kScalerTime = 1.0 / (60.0 * 60.0);
 const int kShuntPin = A0;    // select the input pin for the ADC
 const int kBatteryPin = A1;    // select the input pin for the ADC
-const int kNumReads = 100; // numero di medie
-const int kDelay = 10;
+const int kNumReads = 1000; // numero di medie
+const int kDelay = 1;
+const float kScalerADC = 1.0 / 1023.0;
 
 float avgStartTime = 0.0;
 float avgStopTime = 0.0;
@@ -30,8 +29,31 @@ float Q = 0.0;
 void setup_adc() {
   timestampOffset = millis();
   avgStartTime = timestampOffset;
+  // kScalerADC = readVcc() / 1000.0 / 1023.0;
+  analogReference(INTERNAL);
 }
 
+long readVcc() {
+  long result;
+  // Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA,ADSC));
+  result = ADCL;
+  result |= ADCH<<8;
+  result = 1125300L / result; // Back-calculate AVcc in mV
+  return result;
+}
+
+/**
+ * @brief      Prints a value to serial.
+ *
+ * @param[in]  str        The string in front
+ * @param[in]  value      The value to print
+ * @param[in]  precision  The precision of the printing
+ * @param[in]  unit       The unit of measurement (without \n)
+ */
 void print_val_serial(const char* str, const float value, const int precision,
     const char* unit) {
   Serial.print(str);
@@ -68,12 +90,15 @@ void inline get_avg_read(const int (&adc_mask)[NUM_ADC_PINS],
   for (int i = 0; i < NUM_ADC_PINS; ++i) {
     avg_value[i] = 0.0;
   }
+  // const float scaler = readVcc() / 1000.0 / 1023.0;
+  const float scaler = kScalerADC;
   for (int i = 0; i < num_reads; ++i) {
     for (int j = 0; j < NUM_ADC_PINS; ++j) {
       if (adc_mask[j] > 0) {
         int read_val = analogRead(adc_pins[j]);
-        read_val = analogRead(adc_pins[j]);
-        avg_value[j] += float(read_val) * kScalerADC;
+        // TODO: Check if second read has to be discarded.
+        // read_val = analogRead(adc_pins[j]);
+        avg_value[j] += float(read_val) * scaler;
       }
     }
     delay(read_delay);
@@ -100,14 +125,24 @@ void loop_adc() {
   const float residual_time = get_residual_time(I); // hours
   usedCharge += Q;
   // Print to Serial output
-  print_val_serial("Ah = ", usedCharge * kScalerTime, 6, " Q\t"); // Print Ampere-hour
+  print_val_serial("Ah = ", usedCharge * kScalerTime, 6, " C\t\t"); // Print Ampere-hour
   print_val_serial("V = ", Vbattery, 4, " V\n"); // Print Volt Battery
   // Print to LCD
   print_val_lcd(0, 0, "Ah=", usedCharge * kScalerTime, 2, "");
-  print_val_lcd(0, 1, "V=", Vbattery, 3, "");
+  // print_val_lcd(0, 1, "V=", Vbattery, 3, "");
+  print_val_lcd(0, 1, "Q=", interpolate(Vbattery), 0, "%");
   print_val_lcd(9, 1, "I=", I, 3, "");
-  print_val_lcd(9, 0, "t=", residual_time, 2, "h");
-  // // Store to SD
+  // print_val_lcd(9, 0, "t=", residual_time, 1, "h ");
+  char block = 255;
+  print_val_lcd(9, 0, "t=", residual_time, 1, String("h" + String(block)).c_str());
+  // if (residual_time >= 24) {
+  //   print_val_lcd(9, 0, "t=", residual_time / float(24), 1, "d "); // days
+  // } else if (residual_time < 1) {
+  //   print_val_lcd(9, 0, "t=", residual_time / float(60), 1, "m "); // minutes
+  // } else {
+  //   print_val_lcd(9, 0, "t=", residual_time, 1, String("h" + String(block)).c_str());
+  // }
+  // Store to SD
   print_val_sd(usedCharge * kScalerTime, "\t");
   print_val_sd(Vbattery, "\n");
   // print_bin_val_sd(usedCharge * kScalerTime);
